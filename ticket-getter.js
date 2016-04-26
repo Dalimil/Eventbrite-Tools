fb4087771ceb107e44fef469c94eb9ae1f527548
Dalimil <dalimilhajek@gmail.com>
2016-04-27 00:08:07 +0100

    Beautify code

--- ticket-getter.js
+++ ticket-getter.js
@@ -1,20 +1,19 @@
 /**
-  * This script can be used to get Eventbrite event tickets. You must run it from the Eventbrite event page.
-  * Change 'ticketPositionInList' variable to indicate the position of your desired ticket type in the list (default = 0 is the first ticket type)
-  * Change 'startTime' variable to indicate the time when we should start checking for tickets - typically the time when the tickets are released
-  * If you change 'ticketQuantity' variable above the allowed limit (often anything more than 1), the whole process will fail
-  *
-  * @author Dalimil Hajek
-  * @author Bedis ElAcheche
-  */ 
-
+ * This script can be used to get Eventbrite event tickets. You must run it from the Eventbrite event page.
+ * Change 'ticketPositionInList' variable to indicate the position of your desired ticket type in the list (default = 0 is the first ticket type)
+ * Change 'startTime' variable to indicate the time when we should start checking for tickets - typically the time when the tickets are released
+ * If you change 'ticketQuantity' variable above the allowed limit (often anything more than 1), the whole process will fail
+ *
+ * @author Dalimil Hajek
+ * @author Bedis ElAcheche
+ */
 var ticketPositionInList = 0; // There may be several ticket types - set to 0 to select the first one (or change accordingly)
 var ticketQuantity = 1; // How many tickets you want to buy? - WARNING: Often limited by the event organizer to 1
 var startTime = "2016-04-18T15:59:55"; // When should this script start checking for tickets (e.g. 5 seconds to 4pm)
 //var startTime = ""; // uncomment to start immediately 
 
 function checkLocation() {
-	if(location.href.indexOf("eventbrite") == -1){
+	if (location.href.indexOf("eventbrite") == -1) {
 		console.log("You must go to the Eventbrite event page and run the script from there!");
 		throw new Error("You must go to the Eventbrite event page and run the script from there!");
 	}
@@ -25,14 +24,14 @@ function post(path, params) {
 	form.setAttribute("method", "post");
 	form.setAttribute("action", path);
 
-	for(var key in params) {
-		if(params.hasOwnProperty(key)) {
+	for (var key in params) {
+		if (params.hasOwnProperty(key)) {
 			var hiddenField = document.createElement("input");
 			hiddenField.setAttribute("type", "hidden");
 			hiddenField.setAttribute("name", key);
 			hiddenField.setAttribute("value", params[key]);
 			form.appendChild(hiddenField);
-		 }
+		}
 	}
 	document.body.appendChild(form);
 	form.submit();
@@ -41,7 +40,7 @@ function post(path, params) {
 function findAll(data, s) {
 	var rx = new RegExp(s, "g");
 	var matches = new Array();
-	while((match = rx.exec(data)) !== null){
+	while ((match = rx.exec(data)) !== null) {
 		matches.push(match);
 	}
 	return matches;
@@ -52,17 +51,17 @@ function getTicket(data) {
 	quantityMatches = findAll(data, "quantity_remaining\":([^,]+),");
 	console.log(quantityMatches);
 	console.log(ticketMatches);
-	if(quantityMatches.length != ticketMatches.length){
+	if (quantityMatches.length != ticketMatches.length) {
 		throw new Error("ERROR: No. of ticket types doesn't match ticket quantities data");
 	}
 
 	var ticket = "";
-	for(var i = 0; i < quantityMatches.length; i++){
+	for (var i = 0; i < quantityMatches.length; i++) {
 		var rem = parseInt(quantityMatches[i][1]);
-		if(rem <= 0) continue;
+		if (rem <= 0) continue;
 
 		ticket = ticketMatches[i][1];
-		if(ticketPositionInList <= 0){
+		if (ticketPositionInList <= 0) {
 			break;
 		}
 		ticketPositionInList -= 1; // valid ticket found so untick position
@@ -71,31 +70,35 @@ function getTicket(data) {
 }
 
 var repeat = true;
+
 function run() {
 	checkLocation();
-	$.get(location.href, function( data ) {
+	$.get(location.href, function(data) {
 		// console.log(data);
 		ticket = getTicket(data);
 
-		if(ticket == ""){
-			console.log("Unsuccessful: "+(new Date()).toLocaleTimeString());
-			if(repeat) {
+		if (ticket == "") {
+			console.log("Unsuccessful: " + (new Date()).toLocaleTimeString());
+			if (repeat) {
 				setTimeout(run, 500);
 			}
 			return;
-		} else{
+		} else {
 			var eid = $("form input[name=eid]").attr('value');
-			console.log(ticket +" "+eid);
-			var payload = {'eid': eid, 'has_javascript': 1};
+			console.log(ticket + " " + eid);
+			var payload = {
+				'eid': eid,
+				'has_javascript': 1
+			};
 			payload[ticket] = ticketQuantity;
 			console.log(payload);
 			post("https://www.eventbrite.co.uk/orderstart", payload);
-		}    
+		}
 	});
 }
 
 function stop() {
-	if(repeat == false){
+	if (repeat == false) {
 		return "Already stopped";
 	}
 	repeat = false;
@@ -118,19 +121,19 @@ function msToString(ms) {
 
 function initScheduler() {
 	if (typeof scheduler !== 'undefined') {
-	    clearTimeout(scheduler); // when rerun
+		clearTimeout(scheduler); // when rerun
 	}
 	return null;
 }
 
 var scheduler = initScheduler();
 
-$( document ).ready(function() {
+$(document).ready(function() {
 	checkLocation();
-	
+
 	var timeToStart = getTimeRemaining();
 	console.log("Scheduled start in: " + msToString(timeToStart));
-	scheduler = setTimeout(function(){ run(); }, timeToStart);
+	scheduler = setTimeout(function() {
+		run();
+	}, timeToStart);
 });
-
-
