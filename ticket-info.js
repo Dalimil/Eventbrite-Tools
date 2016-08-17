b14e89088054a3e4bfe9a631afe7d6658164cd3d
Dalimil Hajek <dalimilhajek@gmail.com>
2016-08-17 13:28:34 +0100

    Finish collection parsing for ticket-info.js

--- ticket-info.js
+++ ticket-info.js
@@ -13,12 +13,13 @@ function checkLocation() {
 
 function findCollection(data) {
 	var errorMsg = "Eventbrite changed something and broke my script. Please let me know.";
-	var pattern = /collection.*/g;
+	var pattern = /collection[\s:]+\[{.*/g;
 	var res = data.match(pattern);
 	if(res == null || res.length == 0) {
 		console.log(errorMsg);
 	}
 	var collectionText = '{' + res[0].replace("collection", '"collection"') + '}';
+	collectionText = collectionText.replace(/\t/g, '');
 
 	var result = null;
 	try {
@@ -27,14 +28,43 @@ function findCollection(data) {
 		console.log(errorMsg, err);
 		throw err;
 	}
-	return result;
+	return result.collection;
 }
 
+function parseInteresting(collectionItem) {
+	return {
+		"name": collectionItem.name,
+		"quantity": {
+			"quantity_total": collectionItem.quantity_total,
+			"quantity_sold": collectionItem.quantity_sold,
+			"quantity_remaining": collectionItem.quantity_remaining
+		},
+		"cost": collectionItem.is_free ? "Free" :
+			(collectionItem.total_cost == null ? collectionItem.ticket_price : collectionItem.total_cost.display),
+		"status": {
+			"status_is_sold_out": collectionItem.status_is_sold_out,
+			"status_is_ended": collectionItem.status_is_ended,
+			"on_sale_status": collectionItem.on_sale_status
+		},
+		"dates": {
+			"start": new Date(collectionItem.start_sales).toLocaleFormat(),
+			"end": new Date(collectionItem.end_sales).toLocaleFormat()
+		}
+	};
+}
 
 function run() {
 	checkLocation();
 	var markup = document.documentElement.innerHTML;
 	var collection = findCollection(markup);
+	if(collection == null) return;
+
+	var interestingCollection = [];
+	collection.forEach(function(item) {
+		interestingCollection.push(parseInteresting(item));
+	});
+	console.log(interestingCollection);
+	console.log(JSON.stringify(interestingCollection, null, 2));
 }
 
 run();
